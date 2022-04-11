cb819986e0cd2a8a36c38f0e48ff581f5fe56833
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-04-11 22:08:54 +0200

    add CreateDmParams constructor

--- src/rest/user/create_dm.rs
+++ src/rest/user/create_dm.rs
@@ -8,6 +8,13 @@ pub struct CreateDmParams {
     recipient_id: UserId,
 }
 
+impl CreateDmParams {
+    /// Create new CreateDmParams
+    pub fn new(recipient_id: UserId) -> Self {
+        CreateDmParams { recipient_id }
+    }
+}
+
 /// Generate a send message request
 pub fn create_dm(params: CreateDmParams) -> Request<CreateDmParams> {
     let req = Request::builder()
